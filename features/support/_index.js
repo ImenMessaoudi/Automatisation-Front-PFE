2021a2a75ce648af5a15b22eb5803a2d5000b3ad
imen.messaoudi <imen.messaoudi@esprit.tn>
2024-08-31 11:09:04 +0100

    report Tests

--- features/support/_index.js
+++ features/support/_index.js
@@ -39,8 +39,8 @@ BeforeAll(async function () {
     try {
         console.log("Starting Puppeteer launch...");
         global.browser = await puppeteer.launch({
-            executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
-            //executablePath: '/usr/bin/microsoft-edge',
+            //executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
+            executablePath: '/usr/bin/microsoft-edge',
             headless: false,
             slowMo: 25,
             ignoreHTTPSErrors: true,
